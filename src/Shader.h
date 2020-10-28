2e7ecb96185731c9fbd91f054dff480f073f8ba1
GeoCHiP <x.GeoCHiP.x@yandex.ru>
2020-10-29 02:18:58 +0300

    Feature: new SetUniform1f member-function for shader class

--- src/Shader.h
+++ src/Shader.h
@@ -21,6 +21,7 @@ public:
     int GetUniformLocation(const std::string &name);
 
     void SetUniform1i(const std::string &name, int value);
+    void SetUniform1f(const std::string &name, float value);
     void SetUniform4f(const std::string &name, float v0, float v1, float v2, float v3);
     void SetUniformMat4f(const std::string &name, glm::mat4 matrix);
 
